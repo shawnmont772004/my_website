e20f5f3a9ef96eb4680a1d96a287817ed28aabab
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-06-17 12:56:44 +0530

    install react-router-dom and create pages and routing bw them in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import  Home  from "./pages/Home.jsx"
+import Signin from "./pages/Signin.jsx"
+import Signup from "./pages/Signup.jsx"
+import Profile from './pages/Profile.jsx'
+import About from './pages/About.jsx'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/signin" element={ <Signin />} />
+          <Route path="/signup" element={ <Signup />}/>
+          <Route path="/about" element={ <About />}/>
+          <Route path="/profile" element={ <Profile />}/>
+        </Routes>
+      </Router>
+    </>
+  );
 }
+
+export default App
