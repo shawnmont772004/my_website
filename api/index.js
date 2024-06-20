e86bf5d5564d6a6b9c9aad861c299f2d94ae4cdd
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-06-20 08:17:49 +0530

    set up the database and mongo connectivity

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app= express();
 
 app.listen(3000,() => {
     console.log("Server listening on 3000!");
 });
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to the database!")
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
