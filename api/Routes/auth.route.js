aa370a48a31ecfcdcb39e4e956b4556cb2c277a9
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-06-21 11:47:17 +0530

    sign in api route

--- api/Routes/auth.route.js
+++ api/Routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-import {authcontroller} from "../controllers/auth.controller.js";
+import {authcontroller,authsignincontroller} from "../controllers/auth.controller.js";
 const authRouter=express.Router();
 
 authRouter.post('/signup',authcontroller);
+authRouter.post('/signin',authsignincontroller);
 export default authRouter
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import {errorHandler} from "../utils/error.js";
+
 export const authcontroller= async(req,res,next)=>{
     const {fname,lname,username,email,password,phone}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
@@ -11,6 +13,25 @@ export const authcontroller= async(req,res,next)=>{
     catch(error)
     {
         //res.status(500).json(error);
+        next(error);
+    }
+
+}
+
+export const authsignincontroller=async(req,res,next)=>{
+    const {mail,pass}=req.body;
+    try{
+        const verifyUser= await User.findOne({email : mail});
+    if (!verifyUser){
+        return next(errorHandler(404,"User not found"));
+    }
+    const decryptpass= bcryptjs.compareSync(pass,verifyUser.password);
+    if(!decryptpass){
+        return next(errorHandler(404,"Wrong password"));
+    }
+    res.status(500).json("User logged in successfully");
+    }
+    catch(error){
         next(error);
     }
 
